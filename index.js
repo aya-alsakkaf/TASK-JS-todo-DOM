04cadcb301c24ce648a4f82ef5cc85a1db16588d
Aya-Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2023-12-12 18:30:34 +0300

    JS Done

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ renderTasks(tasks, "tasks-list");
 function taskChecked(taskId, checked) {
   // implement the delete task.
   // You are given the task id
+  tasks.splice(taskId, 1)
+  renderTasks(tasks, "tasks-list");
   console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
 }
 
@@ -27,12 +29,20 @@ function addTask() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
   // continue the code here
+  newID = tasks[tasks.length - 1].id + 1
+  newTask = { id: newID, title: taskTitle, category: selectedCategory, done: false };
+  tasks.push(newTask)
+  console.log(tasks)
+  renderTasks(tasks, "tasks-list");
   alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
   // continue the code here
+  categories.push(newCategory)
+  renderCategories(categories, CATEGORY_SELECTOR)
+  renderCategories(categories, CATEGORY_FILTER)
   alert(`New category was added: ${newCategory}`);
 }
 
@@ -41,5 +51,15 @@ function filterTasks() {
   const done = getFilteredDone();
   // continue the code here
   // REMOVE ME: sample alert
+
+  if (!done) {
+    tasksFiltered = tasks.filter(task => task.category == selectedCategory)
+    console.log(tasksFiltered)
+
+  } else {
+    tasksFiltered = tasks.filter(task => task.category == selectedCategory && task.done == done);
+    console.log(tasksFiltered)
+  }
+  renderTasks(tasksFiltered, "tasks-list");
   alert(`Category: ${selectedCategory} | done: ${done}`);
 }
